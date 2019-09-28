fc69f71eecac36b820c65da136ac4d9dfcbdea9a
Luke Patrick <luke@routerchowder.com>
2019-09-28 17:01:50 -0600

    refactor: Slightly DRYer movement tracking

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -6,18 +6,23 @@ public class RubyController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
+    {}
+
+    private float positionUpdate(float position, float speed, float axis, float time)
     {
-        
+        float newPosition = position + speed * axis * time;
+        return newPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float Speed = 3.0f;
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
-        position.y = position.y + 0.1f * vertical;
+        position.x = positionUpdate(position.x, Speed, horizontal, Time.deltaTime);
+        position.y = positionUpdate(position.y, Speed, vertical, Time.deltaTime);
         transform.position = position;
     }
 }
